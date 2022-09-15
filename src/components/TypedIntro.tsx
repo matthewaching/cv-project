b267ef12a2f823d42643254684c77ec9cc6f5ca9
Matthew Ching <matthewaching@gmail.com>
2022-09-14 22:32:37 -0500

    Add TypeIt module

---
+++ src/components/TypedIntro.tsx
@@ -0,0 +1,11 @@
+import TypeIt from "typeit-react";
+
+function TypedIntro() {
+  return (
+    <div className="App">
+      <TypeIt>This will be typed in a `span` element!</TypeIt>
+    </div>
+  );
+}
+
+export default TypedIntro;
