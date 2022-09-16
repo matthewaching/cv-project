8d4da14a64eeea34bed6fd51a40f71e5a91850ce
Matthew Ching <matthewaching@gmail.com>
2022-09-15 21:52:24 -0500

    Finish typed introduction

--- src/components/TypedIntro.tsx
+++ src/components/TypedIntro.tsx
@@ -1,11 +1,19 @@
 import TypeIt from "typeit-react";
 
+const SuperStrong = ({ children }) => {
+  return <strong className="text-8xl">{children}</strong>;
+};
 function TypedIntro() {
-  return (
-    <div className="App">
-      <TypeIt>This will be typed in a `span` element!</TypeIt>
-    </div>
-  );
-}
+  
+return (
+      <div className="App text-4xl leading-loose">
+        <TypeIt options={{
+          speed: 75,
+        }}>
+          Hi there! My name is<br/><SuperStrong>Matthew Ching</SuperStrong> <br/>and I am a software developer. <br/><br/> Thanks for stopping by.
+        </TypeIt>
+      </div>
+    );
+  };
 
 export default TypedIntro;
